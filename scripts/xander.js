db31e66053afc5c09bc29fae5bfffba29c3da8eb
DrWilliz <xander_1999@yahoo.dk>
2024-10-22 21:57:59 +0200

    stacks list plus search function

---
+++ scripts/xander.js
@@ -0,0 +1,28 @@
+function searchProjects() {
+    const input = document.getElementById('xsearch-input').value.toLowerCase();
+    const rows = document.querySelectorAll('#xprojects tr');
+    
+    rows.forEach(row => {
+        let rowText = row.textContent.toLowerCase();
+        
+        if (rowText.includes(input)) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rows = document.querySelectorAll(".clickable-row");
+
+    
+    rows.forEach(row => {
+        row.addEventListener("click", () => {
+            const projectUrl = row.getAttribute("data-project-url");
+            if (projectUrl) {
+                window.location.href = projectUrl;
+            }
+        });
+    });
+});
