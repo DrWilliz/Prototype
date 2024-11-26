3bc9bc4301a382f8c4d9bb7e0df27732e91a7bf4
Lulu <ldal48848@edu.ucl.dk>
2024-11-26 12:57:09 +0100

    Fik lavede en connection fra prototypen til databasen

--- src/database.js
+++ src/database.js
@@ -5,13 +5,14 @@ import mysql from 'mysql2/promise';
 const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'test',
+  database: 'KubelabDB',
+  password: 'root'
 });
 
 // A simple SELECT query
 try {
   const [results, fields] = await connection.query(
-    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45'
+    'SELECT * FROM `Users`'
   );
 
   console.log(results); // results contains rows returned by server
@@ -21,13 +22,13 @@ try {
 }
 
 // Using placeholders
-try {
-  const [results] = await connection.query(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    ['Page', 45]
-  );
+// try {
+//   const [results] = await connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45]
+//   );
 
-  console.log(results);
-} catch (err) {
-  console.log(err);
-}
+//   console.log(results);
+// } catch (err) {
+//   console.log(err);
+// }
