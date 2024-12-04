2e4d25d699ec24eaf23edbec456c338c3e45d66c
DrWilliz <xander_1999@yahoo.dk>
2024-12-04 17:30:53 +0100

    Database connection in app.js
    I don't even know

--- src/app.js
+++ src/app.js
@@ -1,47 +1,30 @@
 import express from 'express'
 const app = express()
 const PORT = 7000
-// const db = import('/database.js') 
+// const db = import('/database.js')
+import mysql from 'mysql2/promise'
 
-import mysql from 'mysql2/promise';
 const db = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'KubelabDB',
-  password: 'root'
+  password: 'root',
 })
 
-let users = db.query (
-  'SELECT * FROM `users`'
-)
+let users = await db.query('SELECT * FROM `Users`')
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello, World.',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'Goodbye, World.',
-    userId: '2',
-  },
-}
+console.log(users)
+
+let User_ID = await db.query('SELECT User_ID from `Users`')
+
+console.log(User_ID)
 
 app.get('/users', (req, res) => {
   return res.send(Object.values(users))
 })
 
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId])
-})
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages))
-})
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId])
+app.get('/users/:User_ID', (req, res) => {
+  return res.send(users[req.params.User_ID])
 })
 
 app.get('/users', (req, res) => {
--- src/database.js
+++ src/database.js
@@ -1,38 +1,21 @@
 // Get the client
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise'
 
 // Create the connection to database
-module.exports = async () => {
-  const db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'KubelabDB',
-    password: 'root'
-  });
-}
+
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'KubelabDB',
+  password: 'root',
+})
 
 // A simple SELECT query
 try {
-  const [results, fields] = await connection.query(
-    'SELECT * FROM `Users`'
-  );
+  const [results, fields] = await connection.query('SELECT * FROM `Users`')
 
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
+  console.log(results) // results contains rows returned by server
+  console.log(fields) // fields contains extra meta data about results, if available
 } catch (err) {
-  console.log(err);
+  console.log(err)
 }
-
-
-
-// Using placeholders
-// try {
-//   const [results] = await connection.query(
-//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-//     ['Page', 45]
-//   );
-
-//   console.log(results);
-// } catch (err) {
-//   console.log(err);
-// }
