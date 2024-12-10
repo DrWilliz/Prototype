c4684cef45140669c0ae7f22d305682399f91741
DrWilliz <xander_1999@yahoo.dk>
2024-12-10 20:07:10 +0100

    fikset loging

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -29,7 +29,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { ref } from 'vue'
